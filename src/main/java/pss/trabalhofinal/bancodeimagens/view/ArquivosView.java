ef6f3f797690290a778a106b2c056e257ca23014
handaniel <danielhandsantiago@gmail.com>
2022-03-18 18:41:52 -0300

    folder icon added

--- src/main/java/pss/trabalhofinal/bancodeimagens/view/ArquivosView.java
+++ src/main/java/pss/trabalhofinal/bancodeimagens/view/ArquivosView.java
@@ -30,11 +30,11 @@ public class ArquivosView extends javax.swing.JInternalFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 665, Short.MAX_VALUE)
+            .addGap(0, 814, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 364, Short.MAX_VALUE)
+            .addGap(0, 429, Short.MAX_VALUE)
         );
 
         pack();
